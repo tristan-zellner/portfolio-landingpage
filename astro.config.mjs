3aa32d4161c37087d2afe7681cbc6c094954b822
Tristan Zellner <tristan.zellner@posteo.de>
2026-06-10 00:00:47 +0200

    add lebenslauf page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://tristanzellner.de",
   output: "static",
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("/lebenslauf"),
+    }),
+  ],
   fonts: [
     {
       provider: fontProviders.google(),
